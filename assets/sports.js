a7f904e195fdf7ca72b20acdf87f23e2c54b1a3d
wildannz11 <1910631170145@student.unsika.ac.id>
2022-09-01 13:01:58 +0700

    fix code

---
+++ assets/sports.js
@@ -0,0 +1,20 @@
+function changePlayersFootball() {
+    document.getElementById("imgp1").src = "assets/image/football_palyer2.jpeg";
+    document.getElementById("pp1").innerHTML = "Lionel Andrés Messi also known as Leo Messi, (born 24 June 1987) is an Argentine professional footballer who plays as a forward for Ligue 1 club Paris Saint-Germain and is the captain of the Argentina national team.";
+}
+    
+function changePlayersBasketball(){
+    document.getElementById("imgp4").src = "assets/image/basket_player2.jpeg";
+    document.getElementById("pp4").innerHTML = "Earvin Magic Johnson Jr. (born August 14, 1959) is an American former professional basketball player. Often regarded as the greatest point guard of all time, Johnson played 13 seasons in the National Basketball Association (NBA). After winning championships in high school and college, Johnson was selected first overall in the 1979 NBA draft by the Los Angeles Lakers, leading the team to five NBA championships during their Showtime era. Johnson retired abruptly in 1991 after announcing that he had contracted HIV, but returned to play in the 1992 All-Star Game, winning the All-Star MVP Award. After protests from his fellow players, he retired again for four years, but returned in 1996, at age 36, to play 32 games for the Lakers before retiring for the third and final time.";
+}
+    
+function changePlayersVolleyball() {
+    document.getElementById("imgp2").src = "assets/image/Volleyball_player2.jpeg";
+    document.getElementById("pp2").innerHTML = "Sertu Aprilio Perkasa Manganang (born 27 April 1992) is an Indonesian National Army soldier who previously worked as an Indonesian volleyball player who competed as a woman but later transitioned into a man after retirement. She was a member of the Indonesian women's national team. She played in the Thailand-Denmark Women's Volleyball Super League 2019 for Supreme Volley Club as an import player.";
+}
+    
+function changePlayersTennis() {
+    document.getElementById("imgp3").src = "assets/image/tennis_player2.jpeg";
+    document.getElementById("pp3").innerHTML = "Novak Djokovic (Serbian Cyrillic: овак оковић, romanized: Novak oković, pronounced [nôʋaːk dʑôːkoʋitɕ] (listen); born 22 May 1987) is a Serbian professional tennis player. He is currently ranked world No. 6 in singles by the Association of Tennis Professionals (ATP). He has been ranked world No. 1 for a record total 373 weeks,and has finished as the year-end No. 1 a record seven times. He has won 21 Grand Slam men's singles titles, including a record nine Australian Open titles. Overall, he has won 88 ATP singles titles, which include a record 64 Big Titles and a record 38 Masters titles. Djokovic has completed a non-calendar year Grand Slam in singles, becoming the only man in tennis history to be the reigning champion of the four majors at once across three different surfaces. He is also the only player to complete the Career Golden Masters on the ATP Tour, which he has done twice.";
+}
+        
